0884fd453ef2b92b64987aed90495975871e3636
GunterMagno <bcalejandro@iesrafaelalberti.es>
2024-09-27 21:12:57 +0200

    Ejercicio 6 y 7

---
+++ src/ej007.py
@@ -0,0 +1,10 @@
+
+num1 = int(input("Dame un numero: "))
+
+num2 = int(input("Dame otro numero: "))
+
+num3 = int(input("Dame el ultimo numero: "))
+
+total = num1 + num2 + num3
+
+print("La suma de los tres numeros es: ", total)
--- src/ej06.py
+++ src/ej06.py
@@ -1,1 +1,7 @@
-final = int(input("Dame el precio de un articulo con IVA"))
+final = int(input("Dame el precio de un articulo con IVA "))
+
+noiva = (final * 1.10) / 10
+
+impiva = final - noiva
+
+print("El importe sin IVA del articulo es: ",noiva,"y el IVA pagado es: ",impiva)
