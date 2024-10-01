d00e21945bb70a7b9e43ca8c84b059714deb7cf0
GunterMagno <bcalejandro@iesrafaelalberti.es>
2024-10-01 17:28:10 +0200

    ej 16.17.18.19.20 y actualizacion ej4.6.14

--- src/ej04.py
+++ src/ej04.py
@@ -3,4 +3,4 @@ cel = float(input("Dame los grados en Celsius: "))
 
 temp = (cel * 9 / 5) + 32 
 
-print ("Aqui esta la temperatura en Farenheit: ", temp)
+print ("Aqui esta la temperatura en Farenheit: ", round(temp,2))
--- src/ej06.py
+++ src/ej06.py
@@ -1,6 +1,6 @@
-precio = int(input("Dame el precio de un articulo con IVA: "))
+precio = float(input("Dame el precio de un articulo con IVA: "))
 
 siniva = precio / 1.1
 iva = precio - siniva
 
-print("El importe sin IVA del articulo es: ",siniva ,"y el IVA pagado es: ",iva)
+print("El importe sin IVA del articulo es: ",round(siniva,2) ,"y el IVA pagado es: ",round(iva,2))
