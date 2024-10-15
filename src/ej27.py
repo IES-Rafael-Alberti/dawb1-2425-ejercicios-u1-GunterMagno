ba217ee1e32669efeafe4fbc026f5706652bcf27
GunterMagno <bcalejandro@iesrafaelalberti.es>
2024-10-15 13:28:41 +0200

    ej 27,29,30

--- src/ej27.py
+++ src/ej27.py
@@ -1,13 +1,8 @@
 
-precio_unidad = 000000.00
-cantidad_producto = 000
-
 nombre_producto = input("Dame el nombre de un producto: ")
-precio_unidad = precio_unidad + float(input("Dame el precio por unidad del producto: "))
-cantidad_producto = cantidad_producto + int(input("Dame las unidades de producto compradas: "))
+precio_unidad = float(input("Dame el precio por unidad del producto: "))
+cantidad_producto = int(input("Dame las unidades de producto compradas: "))
 
+precio_total = (precio_unidad * cantidad_producto)
 
-precio_total = 00000000.00
-precio_total = precio_total + (precio_unidad * cantidad_producto)
-
-print(f"{nombre_producto} {cantidad_producto} {precio_total}")
+print(f"Nombre del producto: {nombre_producto},precio por unidad; {precio_unidad:06.2f}, cantidad del producto; {cantidad_producto:03d}, precio total; {precio_total:08.2f}")
